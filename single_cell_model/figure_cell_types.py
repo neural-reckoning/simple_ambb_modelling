0a25ef3c92a48f2c835d02d326ed7c154195a4bd
thesamovar <dg.github@thesamovar.net>
2019-06-26 15:58:24 +0100

    Removed some panels from cell types figure

--- single_cell_model/figure_cell_types.py
+++ single_cell_model/figure_cell_types.py
@@ -30,21 +30,25 @@ import numpy
 from simple_model import *
 from model_explorer_jupyter import meshed_arguments
 
+
 def normed(X, *args):
     m = max(amax(abs(Y)) for Y in (X,)+args)
     return X/m
+
 
 # +
 def rmse(x, y, axis=1):
     return sqrt(mean((x-y)**2, axis=axis))
 
+
 def maxnorm(x, y, axis=1):
     return amax(abs(x-y), axis=axis)
+
 
 error_functions = {
     'RMS error': rmse,
     'Max error': maxnorm,
-    }
+}
 # -
 
 latex_parameter_names = dict(
@@ -55,18 +59,21 @@ latex_parameter_names = dict(
     beta=r"$\beta$",
     gamma=r"$\gamma$",
     level=r"$L$ (dB)",
-    )
+)
+
 
 # ...
 
 # +
 def where_close_to_best(mse, max_error):
-    #mse_best = amin(mse, axis=2)
-    #return abs(mse-mse_best[:, :, newaxis])<max_error
+    # mse_best = amin(mse, axis=2)
+    # return abs(mse-mse_best[:, :, newaxis])<max_error
     return mse<max_error
+
 
 def fraction_close_to_best(whereclose):
     return sum(whereclose, axis=2)*1.0/whereclose.shape[2]
+
 
 def desaturate(img, saturation):
     # assume img is rgb shape (M, M, 3)
@@ -74,6 +81,7 @@ def desaturate(img, saturation):
     img[:, :, 1] *= saturation
     img = hsv_to_rgb(img)
     return img
+
 
 def pop_summary(mse, arr, max_error=30, vmin=None, vmax=None):
     # if arr is mse just return the best
@@ -92,16 +100,17 @@ def pop_summary(mse, arr, max_error=30, vmin=None, vmax=None):
     # compute range across close values and all values
     arr_nans = arr.copy()
     arr_nans[~whereclose] = nan
-    arr_std_close = nanmax(arr_nans, axis=2) - nanmin(arr_nans, axis=2)
+    arr_std_close = nanmax(arr_nans, axis=2)-nanmin(arr_nans, axis=2)
     arr_std_all = amax(arr, axis=2)-amin(arr, axis=2)
     saturation = 1-arr_std_close/arr_std_all
     # convert to rgb
-    img = cm.viridis(arr_mean)[:, :, :3] # discard alpha
+    img = cm.viridis(arr_mean)[:, :, :3]  # discard alpha
     # desaturate image
     img = desaturate(img, saturation**2)
     # hide values where nothing is good
     img = dstack((img, sum(whereclose, axis=2)>0))
     return img
+
 
 def plot_cell_types(M, num_params, params,
                     weighted, error_func_name,
@@ -128,11 +137,11 @@ def plot_cell_types(M, num_params, params,
     meanvs = mean(res.raw_measures['vs'], axis=3)
     tMD = res.moddepth['mean']
     rBMF = res.bmf['mean']
-    regions = [('All', mse < 30, 'lightgray', 1),
-               ('Low VS', logical_and(mse < 30, meanvs < 0.75), 'red', 0.25),
-               ('High VS', (mse < 30) & (meanvs >= 0.75), 'blue', 0.25),
-              ]
-    
+    regions = [('All', mse<30, 'lightgray', 1),
+               ('Low VS', logical_and(mse<30, meanvs<0.75), 'red', 0.25),
+               ('High VS', (mse<30)&(meanvs>=0.75), 'blue', 0.25),
+               ]
+
     # Plot the data
     fig = figure(figsize=(10, 6))
     gs_hist = GridSpec(3, 4, left=.05, bottom=0.35, top=1)
@@ -151,7 +160,7 @@ def plot_cell_types(M, num_params, params,
         ('tBMF', (res.bmf['vs'], 4, 64)),
         ('rMD', (res.moddepth['mean'], 0, 1)),
         ('rBMF', (res.bmf['mean'], 4, 64)),
-        ])
+    ])
 
     # Region examples
     ax_lf = subplot(gs_ex[0, 0])
@@ -166,7 +175,7 @@ def plot_cell_types(M, num_params, params,
     env = 0.5*(1-cos(phase))
     region_example_params = {}
     for ax in [ax_lf, ax_hf]:
-        ax.fill_between(phase*180/pi, 0, env, color=(0.9,) * 3, zorder=-2)
+        ax.fill_between(phase*180/pi, 0, env, color=(0.9,)*3, zorder=-2)
         ax.set_xlabel('Phase (deg')
         ax.set_xticks([0, 90, 180, 270, 360])
         ax.set_xlim(0, 360)
@@ -178,7 +187,7 @@ def plot_cell_types(M, num_params, params,
         ax.set_ylim(0, 1)
     seed(54383278)
     for region_name, cond, col, alpha in regions[1:]:
-        print "Region %s contains %.1f%% of good parameters" % (region_name, sum(cond)*100.0/sum(mse<30))
+        print "Region %s contains %.1f%% of good parameters"%(region_name, sum(cond)*100.0/sum(mse<30))
         # Construct parameter values for that region
         example_indices = arange(sum(cond))
         shuffle(example_indices)
@@ -195,27 +204,30 @@ def plot_cell_types(M, num_params, params,
                     values = reshape(res.raw.params[paramname], (M, M, num_params))
                     values = (values[cond]-high)/(high-low)
                     all_values.append(values)
-                all_values = array(all_values) # shape (num_params, sum(cond))
+                all_values = array(all_values)  # shape (num_params, sum(cond))
                 # too large to compute in memory for largest simulation, so use numba
-                #ex_idx = argmin(sum(sum((all_values[:, :, newaxis]-all_values[:, newaxis, :])**2, axis=0), axis=1))
+                # ex_idx = argmin(sum(sum((all_values[:, :, newaxis]-all_values[:, newaxis, :])**2, axis=0), axis=1))
                 output = zeros(all_values.shape[1])
+
                 @numba.jit(nopython=True)
                 def ex_idx_reduction(all_values, output):
                     for i in range(all_values.shape[0]):
                         for j in range(all_values.shape[1]):
                             for k in range(all_values.shape[1]):
                                 output[k] += (all_values[i, j]-all_values[i, k])**2
+
                 ex_idx_reduction(all_values, output)
                 ex_idx = argmin(output)
-            region_params = {}        
+            region_params = {}
             for paramname in params.keys():
                 values = reshape(res.raw.params[paramname], (M, M, num_params))
                 values = values[cond]
                 region_params[paramname] = values[ex_idx]
             cur_res = simple_model(1, region_params, record=['out'])
             cur_res = simple_model_results(1, cur_res, error_func, weighted, interpolate_bmf=True)
-            if cur_ex==num_examples_to_show:
-                print '    most representative example error=%.1f deg, params = %s' % (cur_res.mse[0]*180/pi, region_params)
+            if cur_ex == num_examples_to_show:
+                print '    most representative example error=%.1f deg, params = %s'%(
+                cur_res.mse[0]*180/pi, region_params)
                 region_params['tMTF'] = mean(cur_res.raw_measures['vs'])
                 region_params['tMD'] = cur_res.moddepth['vs'][0]
                 region_params['tBMF'] = cur_res.bmf['vs'][0]
@@ -231,7 +243,6 @@ def plot_cell_types(M, num_params, params,
             ax_rmtf.plot(dietz_fm, cur_res.norm_measures['mean'].T, c=col, alpha=ex_alpha, lw=lw)
             ax_tmtf.plot(dietz_fm, cur_res.raw_measures['vs'].T, c=col, alpha=ex_alpha, lw=lw)
 
-        
     # Histograms
     for i, paramname in enumerate(['alpha', 'beta', 'gamma', 'level', 'taue_ms', 'taui_ms', 'taua_ms',
                                    'tMTF', 'tMD', 'tBMF', 'rMD', 'rBMF']):
@@ -260,11 +271,11 @@ def plot_cell_types(M, num_params, params,
         text(0.02, loc, c, fontsize=14, transform=fig.transFigure,
              horizontalalignment='left', verticalalignment='top')
 
-    
+
 plot_cell_types(
-    #M=20, num_params=100, # 40k parameter sets total, 2m
-    #M=40, num_params=100, # 160k parameter sets total, 5-10m
-    M=80, num_params=500, # 3.2M parameter sets total, several hours
+    # M=20, num_params=100, # 40k parameter sets total, 2m
+    # M=40, num_params=100, # 160k parameter sets total, 5-10m
+    M=80, num_params=500,  # 3.2M parameter sets total, several hours
     num_examples_to_show=10,
     example_alpha=.2,
     weighted=False, error_func_name='Max error',
@@ -272,7 +283,7 @@ plot_cell_types(
         taui_ms=(0.1, 10), taue_ms=(0.1, 10), taua_ms=(0.1, 10),
         level=(-25, 25), alpha=(0, 0.99), beta=(0, 2),
         gamma=(0.1, 1)),
-    )
+)
 
 savefig('figure_cell_types.pdf')
 show()
